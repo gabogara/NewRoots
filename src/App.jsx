529776b3f655612b6f2a95a1b3afe6e87c9e5763
gabogara <gabo1gara@gmail.com>
2026-04-24 19:30:12 -0700

    feat: add Navbar component with links

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
+    <main>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </div>
+    </main>
   );
 };
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { NavLink } from "react-router";
+
+export const Navbar = () => {
+  return (
+    <aside>
+      <nav>
+        <NavLink to="/">
+          New <span>Root</span>
+        </NavLink>
+
+        <NavLink to="/create">Create a</NavLink>
+
+        <NavLink to="/posts/new">Create Post</NavLink>
+      </nav>
+    </aside>
+  );
+};
+
+export default Navbar;
